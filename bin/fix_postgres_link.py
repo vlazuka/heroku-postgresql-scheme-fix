f44ac82888155af2ac13b5777cbb3c448a518cdc
Vitalii Lazuka <vlazuka@salesforce.com>
2023-01-09 13:08:16 -0800

    Procfile fix 3

--- bin/fix_postgres_link.py
+++ bin/fix_postgres_link.py
@@ -5,5 +5,7 @@ import re
 for k, v in sorted(os.environ.items()):
     if v.startswith("postgres://"):
         print(k, v)
-        os.system("{0}=\"{1}\"".format(k, re.sub(r"postgres://", "postgresql://", v, 1)))
+        # print("export {0}=\"{1}\"".format(k, re.sub(r"postgres://", "postgresql://", v, 1)))
+        os.system("export {0}=\"{1}\"".format(k, re.sub(r"postgres://", "postgresql://", v, 1)))
+        print(os.environ[k])
         # os.environ[k] = re.sub(r"postgres://", "postgresql://", v, 1)
