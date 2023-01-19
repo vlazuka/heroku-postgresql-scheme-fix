f3281049486e75c2ee721934c00e738b05ad8929
Vitalii Lazuka <vlazuka@salesforce.com>
2023-01-18 23:31:58 -0800

    te

--- .profile.d/test.sh
+++ .profile.d/test.sh
@@ -4,18 +4,11 @@ export_env_dir() {
   postgresql="postgresql://"
   acceptlist_regex=${2:-''}
   denylist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
-  echo "t1"
-  if [ -d "$env_dir" ]; then
-    echo "t2"
-    for e in $(ls $env_dir); do
-      echo "t3"
-      echo "$e" | grep -E "$acceptlist_regex" | grep -qvE "$denylist_regex" &&
-      export "$e=${$(cat $env_dir/$e)/"$postgres"/"$postgresql"}"
-      :
-    done
-  fi
+  for e in $(ls $env_dir); do
+    echo "$e" | grep -E "$acceptlist_regex" | grep -qvE "$denylist_regex" &&
+    export "$e=${$(cat $env_dir/$e)/"$postgres"/"$postgresql"}"
+    :
+  done
 }
 
-echo "hi"
 export_env_dir
-echo "bye"
