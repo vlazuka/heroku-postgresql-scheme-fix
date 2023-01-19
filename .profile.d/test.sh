43af77de28c4bf6dcb0f51969d04aca91456dcc3
Vitalii Lazuka <vlazuka@salesforce.com>
2023-01-19 10:04:26 -0800

    fixed script

--- .profile.d/test.sh
+++ .profile.d/test.sh
@@ -1,13 +1,8 @@
-export_env_dir() {
-  env_dir=$1
-  postgres="postgres://"
-  postgresql="postgresql://"
-  acceptlist_regex=${2:-''}
-  denylist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
-  for e in $(ls $env_dir); do
-    echo "$e" | grep -E "$acceptlist_regex" | grep -qvE "$denylist_regex" 
-    :
-  done
-}
+unset IFS
+postgres="postgres://"
+postgresql="postgresql://"
 
-export_env_dir
+for var in $(compgen -e); do
+    export $var=${!var/"$postgres"/"$postgresql"}
+    echo $"$var=${!var}"
+done
