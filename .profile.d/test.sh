6edaa850881cd3494076bec10f2c18aebbdf58fa
Vitalii Lazuka <vlazuka@salesforce.com>
2023-01-19 00:15:56 -0800

    test

--- .profile.d/test.sh
+++ .profile.d/test.sh
@@ -5,8 +5,7 @@ export_env_dir() {
   acceptlist_regex=${2:-''}
   denylist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
   for e in $(ls $env_dir); do
-    echo "$e" | grep -E "$acceptlist_regex" | grep -qvE "$denylist_regex" &&
-    export "$e=${(cat $env_dir/$e)/"$postgres"/"$postgresql"}"
+    echo "$e" | grep -E "$acceptlist_regex" | grep -qvE "$denylist_regex" 
     :
   done
 }
